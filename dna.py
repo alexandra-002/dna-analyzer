d7cb23ad78912adf04fa6138eb29229b1045cfd7
Alexandra Sigmon <alisigmon@gmail.com>
2026-07-07 10:28:25 -0400

    Add DNA sequence validation

--- dna.py
+++ dna.py
@@ -1,19 +1,29 @@
 
 sequence = input("Enter a DNA sequence: ").upper()
 
-length = len(sequence)
+valid_bases = "ATCG"
+is_valid = True
 
-g_count = sequence.count("G")
-c_count = sequence.count("C")
-a_count = sequence.count("A")
-t_count = sequence.count("T")
+for base in sequence:
+	if base not in valid_bases:
+		is_valid = False
 
-gc_content = ((g_count + c_count) / length) * 100
+if not is_valid:
+	print("Invalid DNA sequence")
+else:
+	length = len(sequence)
 
-print("Sequence:", sequence)
-print("Length:", length)
-print("GC Content:", round(gc_content, 2), "%")
-print("A:", a_count)
-print("T:", t_count)
-print("C:", c_count)
-print("G:", g_count)
+	g_count = sequence.count("G")
+	c_count = sequence.count("C")
+	a_count = sequence.count("A")
+	t_count = sequence.count("T")
+
+	gc_content = ((g_count + c_count) / length) * 100
+
+	print("Sequence:", sequence)
+	print("Length:", length)
+	print("GC Content:", round(gc_content, 2), "%")
+	print("A:", a_count)
+	print("T:", t_count)
+	print("C:", c_count)
+	print("G:", g_count)
