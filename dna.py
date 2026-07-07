f480ecddec6a7220e6e4bd0c7f18f64841700874
Alexandra Sigmon <alisigmon@gmail.com>
2026-07-07 10:13:09 -0400

    Add GC content calculation

--- dna.py
+++ dna.py
@@ -1,7 +1,13 @@
 
-sequence = input("Enter a DNA sequence: ")
+sequence = input("Enter a DNA sequence: ").upper()
 
 length = len(sequence)
 
+g_count = sequence.count("G")
+c_count = sequence.count("C")
+
+gc_content = ((g_count + c_count) / length) * 100
+
 print("Sequence:", sequence)
 print("Length:", length)
+print ("GC Content:", round(gc_content, 2), "%")
