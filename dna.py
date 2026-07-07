3a454b34691068e80b0cc0a1e71fdd3fa55305da
Alexandra Sigmon <alisigmon@gmail.com>
2026-07-07 10:16:59 -0400

    Add nucleotide counting and GC content analysis

--- dna.py
+++ dna.py
@@ -5,9 +5,15 @@ length = len(sequence)
 
 g_count = sequence.count("G")
 c_count = sequence.count("C")
+a_count = sequence.count("A")
+t_count = sequence.count("T")
 
 gc_content = ((g_count + c_count) / length) * 100
 
 print("Sequence:", sequence)
 print("Length:", length)
-print ("GC Content:", round(gc_content, 2), "%")
+print("GC Content:", round(gc_content, 2), "%")
+print("A:", a_count)
+print("T:", t_count)
+print("C:", c_count)
+print("G:", g_count)
